09b8985d2a8052dbaf4a5a3ef9491c4ca65e7520
Sandhan Meher <codewithsandhan@gmail.com>
2024-03-15 15:19:34 +0530

    suer /student name roll api ..

---
+++ routes/user.js
@@ -0,0 +1,69 @@
+const express=require("express")
+const router = express.Router()
+
+const {StudentCollection}=require("../model/studentSchema")
+
+router.use((req,res,next)=>{
+    console.log("Time : ",Date.now());
+    next()
+})
+
+// add user (student )
+router.post("/user",async (req,res)=>{
+    const data=new StudentCollection(req.body)
+    const a= await data.save();
+    console.log(`router.post("/user")`)
+    res.status(200).send(a);
+})
+
+
+// get user(all students)
+router.get("/user",async (req,res)=>{
+    const students=await StudentCollection.find({});
+    console.log(`router.get("/user")`)
+    console.log(students)
+    res.send(students);
+    
+})
+
+
+// get student/user by id
+router.get("/user:id",async (req,res)=>{
+   try{
+    const _id=req.params.id;
+    console.log(_id)
+    const students=await StudentCollection.findById(_id);
+    console.log(`router.get("/user:id")`)
+    console.log(students)
+    res.status(200).send(students);
+   }catch(er){
+    console.log(er)
+   }
+})
+
+
+// update user by id 
+router.put("/user:id",async (req,res)=>{
+    const _id=req.params.id;
+    
+    const a=await StudentCollection.findByIdAndUpdate(_id,req.body,{
+        new:true
+    })
+    console.log(a);
+    res.status(200).send("updated"+a);
+})
+
+
+// remove student from .....
+router.delete("/user:id",async (req,res)=>{
+    const _id=req.params.id;
+    const a=await StudentCollection.findByIdAndDelete(_id);
+    console.log(a)
+    if(a){
+        res.status(200).send("deleted"+a);
+    }
+    res.send("not present");
+})
+
+
+module.exports={router};
--- server.js
+++ server.js
@@ -6,38 +6,10 @@ const app=express()
 
 require("./model/connection")
 const {StudentCollection}=require("./model/studentSchema")
-const { userInfo } = require("os")
+const {router}=require("./routes/user")
 
 app.use(express.json());
-
-
-app.post("/user",async (req,res)=>{
-    const data=new StudentCollection(req.body)
-    const a= await data.save();
-    console.log(`app.post("/user")`)
-    res.status(200).send(a);
-})
-
-app.get("/user",async (req,res)=>{
-    const students=await StudentCollection.find({});
-    console.log(`app.get("/user")`)
-    console.log(students)
-    res.send(students);
-    
-})
-
-// (async ()=>{
-//     const a1=new StudentCollection({
-//         "name":"a",
-//         "rollno":"1"
-//     })
-//     const a=await a1.save();
-//     console.log(a)
-// })().then(()=>{
-//     console.log("one record.....")
-// }).catch((er)=>{
-//     console.log(er);
-// })
+app.use(router)
 
 
 const port =process.env.port || 1000;
