86e0c34f736b0287601e4b3d124175c028508f9c
Sandhan Meher <codewithsandhan@gmail.com>
2024-03-17 01:06:54 +0530

    registation form created

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const path=require("path")
 const mongoose=require("mongoose")
+const bodyParser=require("body-parser")
 
 const app=express()
 
@@ -9,11 +10,24 @@ const {StudentCollection}=require("./model/studentSchema")
 const {router}=require("./routes/user")
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.set("view engine","ejs")  
 app.use(router)
-app.set("view engine","ejs")
 
 app.get("/",(req,res)=>{
-    res.render("index")
+    res.render("index",{registation:"registation"})
+})
+
+app.get("/registation",(req,res)=>{
+    res.render("registation")
+})
+
+app.post("/submit",async (req,res)=>{
+    const s1=new StudentCollection(req.body);
+     await s1.save();
+
+    res.render("index",{registation:"registered"})
+
 })
 
 app.use("*",(req,res,next)=>{
